65475a16f0524d27f14c9b7e0cc3dc35321db17b
ropi <araujobrodrigo@gmail.com>
2020-09-20 18:17:30 -0500

    File sharing

---
+++ src/us/uofm/comp/Client.java
@@ -0,0 +1,74 @@
+package us.uofm.comp;
+
+import java.net.*;
+import java.io.*;
+
+public class Client {
+	
+	public static int port = 4999;
+	public static String server = "127.0.0.1";
+	
+	public static String fileSave = "testDownloaded.txt";
+	public static int fileSize = 888888;	//is this necessary?
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		FileOutputStream fos = null;
+		BufferedOutputStream bos = null;
+		InputStream is = null;
+		
+		int bytesRead = 0;
+		int currentBytes = 0;
+		
+		Socket s = null;
+		
+		try {
+		
+			s = new Socket("localhost", port);
+			System.out.println("Looking for host...");
+			
+			byte byteArray[] = new byte[fileSize];
+			is = s.getInputStream();
+			
+			fos = new FileOutputStream(fileSave);
+			bos = new BufferedOutputStream(fos);
+			
+			bytesRead = is.read(byteArray, 0, byteArray.length);
+			currentBytes = bytesRead;
+			
+			do {
+				bytesRead = is.read(byteArray, currentBytes, (byteArray.length - currentBytes));
+				
+				if(bytesRead >= 0) {
+					currentBytes += bytesRead;
+				}
+			} while(bytesRead > -1);
+			System.out.println("BBBBBBBBBBB");
+			bos.write(byteArray, 0, currentBytes);
+			bos.flush();
+			
+			System.out.println("File downloaded: " + fileSave + " (" + currentBytes + " bytes received)");
+			
+		
+			
+			
+			PrintWriter pr = new PrintWriter(s.getOutputStream());
+			pr.println("hola");
+			pr.flush();
+			
+			InputStreamReader in = new InputStreamReader(s.getInputStream());
+			BufferedReader bf = new BufferedReader(in);
+			
+			String str = bf.readLine();
+			System.out.println("Server: " + str);
+			
+		} finally {
+			if (s != null) s.close();
+			if (fos != null) fos.close();
+		    if (bos != null) bos.close();
+		}
+		
+		
+	}
+}
---
+++ src/us/uofm/comp/Host.java
@@ -0,0 +1,71 @@
+package us.uofm.comp;
+
+import java.net.*;
+import java.io.*;
+
+
+public class Host {
+	
+	public static String fileP = "test.txt";
+	
+	public static int port = 4999;
+	
+	public static void main(String[] args) throws IOException {
+		
+		ServerSocket ss = new ServerSocket(port);
+		
+		FileInputStream fis = null;
+	    BufferedInputStream bis = null;
+	    OutputStream outputS = null;
+	    Socket s = null;
+	    
+		//while(true) {
+			System.out.println("Attempting to find client...");
+			
+			try {
+				s = ss.accept();
+				
+				System.out.println("Client connected! Connected to: " + s);
+				
+				File file = new File (fileP);
+				
+				byte [] bytearray  = new byte [(int)file.length()];
+				
+				fis = new FileInputStream(file);
+				bis = new BufferedInputStream(fis);
+				bis.read(bytearray,0,bytearray.length);
+				outputS = s.getOutputStream();
+				
+				System.out.println("Sending file: " + file + "(" + bytearray.length + "bytes)");
+				
+				outputS.write(bytearray,0,bytearray.length);
+				outputS.flush();
+				
+				
+				
+				
+				//InputStreamReader in = new InputStreamReader(s.getInputStream());
+				//BufferedReader bf = new BufferedReader(in);
+				
+				//String str = bf.readLine();
+				//System.out.println("Client: " + str);
+				
+				//PrintWriter pr = new PrintWriter(s.getOutputStream());
+				//pr.println("que pasa? Yo soy el host");
+				//pr.flush();
+				
+			}
+			finally{
+				if (bis != null) bis.close();
+		        if (outputS != null) outputS.close();
+		        if (s !=null) s.close();
+		        if (ss !=null) ss.close();
+			}
+			
+		//}
+		
+		
+		
+		
+	}
+}
