a9ebac639dc5d39547c81b49939b638a9c8132bf
ropi <araujobrodrigo@gmail.com>
2020-11-02 16:35:34 -0600

    Sync v3

--- src/us/uofm/comp/ClientListener.java
+++ src/us/uofm/comp/ClientListener.java
@@ -5,11 +5,16 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
+import org.apache.commons.net.ntp.TimeStamp;
 
 import javafx.scene.control.Label;
 import javafx.scene.media.MediaPlayer;
@@ -25,6 +30,12 @@ public class ClientListener extends Thread/* implements Runnable */ {
 	private Socket ntpSocket = null;
 	private String incomingS = null;
 	private BufferedReader br = null;
+	private BufferedReader br2 = null;
+
+	private NTPUDPClient newtClient;
+	private InetAddress inetAddress;
+	private volatile TimeInfo timeInfo;
+	private volatile Long offset;
 
 	private boolean quit = false;
 
@@ -40,7 +51,7 @@ public class ClientListener extends Thread/* implements Runnable */ {
 	public void run() {
 
 		cplayer = new ClientPlayer(mp);
-		
+
 		try {
 			is = s.getInputStream();
 		} catch (IOException e1) {
@@ -50,46 +61,57 @@ public class ClientListener extends Thread/* implements Runnable */ {
 		br = new BufferedReader(isr);
 
 		cplayer.start();
-		
+
+		newtClient = new NTPUDPClient();
+		newtClient.setDefaultTimeout(10_000);
+		try {
+			inetAddress = InetAddress.getByName("pool.ntp.org");
+		} catch (UnknownHostException e1) {
+			e1.printStackTrace();
+		}
+
 		while (!quit) {
 			try {
-				//use a switch?
+				// use a switch?
 				incomingS = br.readLine();
-				
+
 				String timestamp = getTime();
 				boolean skip = false;
-				
-				//WORK ON CORRECTING THE DESYNCHRONIZATION WHEN FAILING TO GET TIME
+
+				// WORK ON CORRECTING THE DESYNCHRONIZATION WHEN FAILING TO GET TIME?
+
 				Timestamp currJavaTime = new Timestamp(new java.util.Date().getTime());
-				while(timestamp == null) {
-					waitSync(1);
-					System.out.println("Time request failed. Trying again");
-					timestamp = getTime();
-					skip = true;
-				}
+				//while (timestamp == null) {
+					//waitSync(2);
+					//System.out.println("Time request failed. Trying again");
+					//timestamp = getTime();
+					//skip = true;
+				//}
 				Timestamp newJavaTime = new Timestamp(new java.util.Date().getTime());
 				double msToSkip = newJavaTime.getTime() - currJavaTime.getTime();
+				//Mon, Nov 02 2020 15:43:51.135
+				timestamp = timestamp.split(" ")[4];
+				System.out.println("Atomic time is: " + timestamp);
 				
-				
-				timestamp = timestamp.split(" ")[2];
-				System.out.println("Nist says the time is: " + timestamp);
-				int waitTime = Integer.parseInt(timestamp.split(":")[2]) % 3;
-				waitSync(waitTime);
-				
+				double waitTime = Double.parseDouble(timestamp.split(":")[2]) % 2;
+
 				if (incomingS.contains("play")) {
-					if(skip)
+					if (skip)
 						cplayer.skipPlay(msToSkip);
-					else
+					else {
+						waitSync(waitTime);
 						cplayer.play();
-				} else if(incomingS.contains("pause")) {
+					}
+				} else if (incomingS.contains("pause")) {
+					waitSync(waitTime);
 					cplayer.pause();
-				} else if(incomingS.contains("stop")) {
+				} else if (incomingS.contains("stop")) {
+					waitSync(waitTime);
 					cplayer.stopMusic();
-				} else if(incomingS.contains("quit")) {
+				} else if (incomingS.contains("quit")) {
 					stopListener();
 					continue;
 				}
-
 
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -112,32 +134,45 @@ public class ClientListener extends Thread/* implements Runnable */ {
 		}
 
 	}
-	
-	private static void waitSync(int seconds) {
+
+	private static void waitSync(double seconds) {
 		try {
-			if(seconds == 0) {
-				seconds++;
-			}
-			sleep(seconds + 1000);
+			//if (seconds == 0) {
+			//	seconds++;
+			//}
+			int time = (int) (seconds * 1000);
+			sleep((long)time);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private String getTime() {
 		String time = null;
 		try {
-			ntpSocket = new Socket("time.nist.gov", 13);
+			TimeInfo timeInfo = newtClient.getTime(inetAddress);
+			timeInfo.computeDetails();
+			if (timeInfo.getOffset() != null) {
+				this.timeInfo = timeInfo;
+				this.offset = timeInfo.getOffset();
+			}
+
+			// The current client system time. Should this be used?
+			TimeStamp systemNtpTime = TimeStamp.getCurrentTime();
+			String sysTime = systemNtpTime.toDateString();
+			System.out.println("System time:\t" + sysTime);
+
+			// Calculating the remote server NTP time. Atomic time
+			long currentTime = System.currentTimeMillis();
+			TimeStamp atomicNtpTime = TimeStamp.getNtpTime(currentTime + offset);
+			String atomicTime = atomicNtpTime.toDateString();
+			System.out.println("Atomic time:\t" + atomicTime);
+			
+			time = atomicTime;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		try {
-			BufferedReader br2 = new BufferedReader(new InputStreamReader(ntpSocket.getInputStream()));
-			br2.readLine();
-			time = br2.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+
 		return time;
 	}
 
--- src/us/uofm/comp/ClientPlayer.java
+++ src/us/uofm/comp/ClientPlayer.java
@@ -1,5 +1,7 @@
 package us.uofm.comp;
 
+import org.apache.commons.net.ntp.TimeStamp;
+
 import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
 
@@ -17,6 +19,9 @@ public class ClientPlayer extends Thread {
 	}
 	
 	public void play() {
+		TimeStamp systemNtpTime = TimeStamp.getCurrentTime();
+		String sysTime = systemNtpTime.toDateString();
+		System.out.println("I'M PLAYING AT THIS TIME:\t" + sysTime);
 		mp.play();
 	}
 	
