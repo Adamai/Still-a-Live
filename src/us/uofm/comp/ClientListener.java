bc071983eabbf1483b4b7d3a872f5af24bb3eb36
ropi <araujobrodrigo@gmail.com>
2020-11-04 23:26:54 -0600

    Reverting to previous NTP

--- src/us/uofm/comp/ClientListener.java
+++ src/us/uofm/comp/ClientListener.java
@@ -76,15 +76,12 @@ public class ClientListener extends Thread/* implements Runnable */ {
 			try {
 				incomingS = br.readLine();
 
-				//String timestamp = getTime();
+				String timestamp = getTime();
 
 				//Mon, Nov 02 2020 15:43:51.135
-				//timestamp = timestamp.split(" ")[4];
+				timestamp = timestamp.split(" ")[4];
 				
-				String timestamp = getTime2();
-				//04-nov-2020 22:48:46,212000
-				timestamp = timestamp.replace(',', '.');
-				timestamp = timestamp.split(" ")[1];
+				
 				
 				System.out.println("Atomic time is: " + timestamp);
 
@@ -157,50 +154,6 @@ public class ClientListener extends Thread/* implements Runnable */ {
 			e.printStackTrace();
 		}
 		return time;
-	}
-
-	private String getTime2() throws IOException {
-		// Send request
-		DatagramSocket socket = new DatagramSocket();
-		InetAddress address = inetAddress;
-		byte[] buf = new NtpMessage().toByteArray();
-		DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 123);
-
-		// Set the transmit timestamp *just* before sending the packet
-		// ToDo: Does this actually improve performance or not?
-		NtpMessage.encodeTimestamp(packet.getData(), 40, (System.currentTimeMillis() / 1000.0) + 2208988800.0);
-
-		socket.send(packet);
-
-		// Get response
-		System.out.println("NTP request sent, waiting for response...\n");
-		packet = new DatagramPacket(buf, buf.length);
-		socket.receive(packet);
-
-		// Immediately record the incoming timestamp
-		double destinationTimestamp = (System.currentTimeMillis() / 1000.0) + 2208988800.0;
-
-		// Process response
-		NtpMessage msg = new NtpMessage(packet.getData());
-
-		// Corrected, according to RFC2030 errata
-		double roundTripDelay = (destinationTimestamp - msg.originateTimestamp)
-				- (msg.transmitTimestamp - msg.receiveTimestamp);
-
-		double localClockOffset = ((msg.receiveTimestamp - msg.originateTimestamp)
-				+ (msg.transmitTimestamp - destinationTimestamp)) / 2;
-
-		// Display response
-		System.out.println(msg.toString());
-
-		System.out.println("Dest. timestamp:     " + NtpMessage.timestampToString(destinationTimestamp));
-
-		System.out.println("Round-trip delay: " + new DecimalFormat("0.00").format(roundTripDelay * 1000) + " ms");
-
-		System.out.println("Local clock offset: " + new DecimalFormat("0.00").format(localClockOffset * 1000) + " ms");
-
-		socket.close();
-		return NtpMessage.timestampToString(destinationTimestamp);
 	}
 
 }
